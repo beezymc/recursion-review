bcad72d78307045ea48dbc2d193cd8113206da8a
David Rajec <david.rajec@gmail.com>
2021-11-08 12:31:03 -0800

    Partially thru stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,27 @@
 
 // but you don't so you're going to write it from scratch:
 
+var handlePrimitive = function(primitive) {
+  if (Number.isInteger(primitive)) {
+    return primitive + '';
+  } else if (primitive === null) {
+    return 'null';
+  } else if (typeof primitive === 'boolean') {
+    return primitive + '';
+  } else if (typeof primitive === 'string') {
+    return '\"' + primitive + '\"';
+  }
+};
+
 var stringifyJSON = function(obj) {
+  var returnStr = '';
+  if (typeof obj !== 'object') {
+    returnStr += handlePrimitive(obj);
+  }
+
+  var returnStr = 'TBD';
   // your code goes here
+  // primitives: Numbers, booleans, strings, null...
+  // objects and arrays
+  // ^Recursion
 };

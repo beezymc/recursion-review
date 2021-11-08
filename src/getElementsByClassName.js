8f09435d56276afb00cc5a63a889f17978761833
David Rajec <david.rajec@gmail.com>
2021-11-08 12:11:07 -0800

    Got getElementsByClassName functional.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,27 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var body = document.body;
+  var bodyClass = body.className;
+  var bodyClassArray = bodyClass.split(' ');
+  var results = [];
+  for (var i = 0; i < bodyClassArray.length; i++) {
+    if (bodyClassArray[i] === className) {
+      results.push(body);
+    }
+  }
+  var recurThruElements = function(node) {
+    for (var i = 0; i < node.children.length; i++) {
+      var childClassArray = node.children[i].className.split(' ');
+      for (var j = 0; j < childClassArray.length; j++) {
+        if (childClassArray[j] === className) {
+          results.push(node.children[i]);
+        }
+      }
+      recurThruElements(node.children[i]);
+    }
+  };
+  recurThruElements(body);
+  console.log(results);
+  return results;
 };

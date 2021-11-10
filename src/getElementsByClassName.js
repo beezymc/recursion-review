f25dacf3082c0ddab39fea41d8205859c35b35f9
David Rajec <david.rajec@gmail.com>
2021-11-09 19:14:22 -0800

    Completed Bare Minimum Requirements and began a ParseJSON attempt.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,46 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  var body = document.body;
-  var bodyClass = body.className;
-  var bodyClassArray = body.classList;
+// var getElementsByClassName = function(className
+// ) {
+//   var body = document.body;
+//   var bodyClass = body.className;
+//   var bodyClassArray = body.classList;
+//   var results = [];
+//   for (var i = 0; i < bodyClassArray.length; i++) {
+//     if (bodyClassArray[i] === className) {
+//       results.push(body);
+//     }
+//   }
+//   var recurThruElements = function(node) {
+//     for (var i = 0; i < node.children.length; i++) {
+//       var childClassArray = node.children[i].classList;
+//       for (var j = 0; j < childClassArray.length; j++) {
+//         if (childClassArray[j] === className) {
+//           results.push(node.children[i]);
+//         }
+//       }
+//       recurThruElements(node.children[i]);
+//     }
+//   };
+//   recurThruElements(body);
+//   return results;
+// };
+
+//attempted (and completed) implementation w/o side effects
+//check if current node has the classname. If so, push it to results.
+//recur through children, concatting results upon return.
+var getElementsByClassName = function(className, node) {
+  node = node || document.body;
+  var nodeClassArray = node.classList;
   var results = [];
-  for (var i = 0; i < bodyClassArray.length; i++) {
-    if (bodyClassArray[i] === className) {
-      results.push(body);
+  for (var i = 0; i < nodeClassArray.length; i++) {
+    if (nodeClassArray[i] === className) {
+      results.push(node);
     }
   }
-  var recurThruElements = function(node) {
-    for (var i = 0; i < node.children.length; i++) {
-      var childClassArray = node.children[i].classList;
-      for (var j = 0; j < childClassArray.length; j++) {
-        if (childClassArray[j] === className) {
-          results.push(node.children[i]);
-        }
-      }
-      recurThruElements(node.children[i]);
-    }
-  };
-  recurThruElements(body);
+  for (var i = 0; i < node.children.length; i++) {
+    results = results.concat(getElementsByClassName(className, node.children[i]));
+  }
   return results;
 };
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,57 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
-
 // but you're not, so you'll write it from scratch:
+
+// Specs:
+// input: string
+// output: Object, Array, string, number, boolean, or null depending on the string.
+// constraints: should receive valid JSON, will not accept trailing commas, doesn't allow single quotes.
+// edge cases: functions aren't allowed in JSON -> functions should remain strings if present.
+
+// Justification:
+// The intention for this function is to take a string and convert it into JSON.
+
+// Visualization: see parseJSON.drawio
+
+// Approximation:
+// Take our string and begin looping through each character.
+// If we hit a primitive type (string, number, boolean, or null), we parse that type and return it (explained below).
+// For nulls or booleans, check if the completed string is equal to 'true', 'false', or 'null', and return that value.
+// For numbers, check if the completed string length is equal to that string converted to an integer and back to a string. (This should account for any stray characters within a number). If it passes, return the integer.
+// If all the above fail, return the string.
+// If we hit a non-primitive, first we check if it's an array or an object.
+// If an array, we parse the primitive(s) and arrays/objects within it
+// Keep looping thru characters until we hit a closing bracket, keeping track of the opening bracket index.
+// Recur the slice of characters from the opening bracket to the closing bracket, enclosing the result in array brackets.
+// If an object, we parse the primitive(s) and arrays/objects within it
+// Keep looping thru characters until we hit a closing brace, keeping track of the opening brace index.
+// Recur the slice of characters from the opening bracket to the closing bracket, enclosing the result in object braces.
+
+// Current solution doesn't account for key/value pairs, commas.
+
+// var parsePrimitive = function (primitive) {
+//   if (primitive === 'null') {
+//     return null;
+//   } else if (primitive === 'true') {
+//     return true;
+//   } else if (primitive === 'false') {
+//     return false;
+//   } else {
+//     var number = parseInt(primitive);
+//     var restrung = number + '';
+//     if (restrung.length === primitive.length) {
+//       return number;
+//     } else {
+//       return primitive;
+//     }
+//   }
+// };
+
+//solve a simple case
+//json = '{"name":"John", "age":30, "city":"New York"}'
 var parseJSON = function(json) {
-  // your code goes here
+//   // your code goes here
+//   for (var i = 0; i < json.length; i++) {
+//     if (json[i] === '{')
+//   }
 };
